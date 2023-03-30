1751d754014c977762aa41a99fc10960e779eb55
samiraya23 <samiraya23@hotmail.com>
2023-03-29 21:21:30 -0600

    Borrando para dar pie al proyecto  bueno

---
+++ Desktop/ESCOM/Koyotito/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { PostsComponent } from './modules/posts/posts.component';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DefaultModule } from './layouts/default/default.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DefaultModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
