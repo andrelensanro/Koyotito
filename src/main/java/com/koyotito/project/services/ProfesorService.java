59c9e0f3cdd87d5f5611b39ec629ec84b697b1d4
andrelensanro <andrea1selene0sanchez.robles@gmail.com>
2023-04-16 16:52:32 -0600

    Implementacion de repositorio, services en las clases que intervienen en el registro y login de usuario tipo Tutor y Profesor. Le agregue al model de Profesor el atributo institucion.

---
+++ src/main/java/com/koyotito/project/services/ProfesorService.java
@@ -0,0 +1,18 @@
+package com.koyotito.project.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.koyotito.project.model.Profesor;
+import com.koyotito.project.repository.ProfesorRepo;
+
+@Service
+public class ProfesorService {
+	@Autowired
+	private ProfesorRepo profesorRepo; 
+	
+	public Profesor save(Profesor profesor) {
+		return profesorRepo.save(profesor);
+	}
+	
+}
